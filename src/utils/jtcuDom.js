42f57407c7ce821c59d41df2fc3942beb650dacb
mr21 <thomastortorini@gmail.com>
2025-11-02 22:13:34 +0100

    jtcuDom: add private prefix

--- src/utils/jtcuDom.js
+++ src/utils/jtcuDom.js
@@ -1,29 +1,29 @@
-const jtcu_dom_resizeMap = new Map();
-const jtcu_dom_resizeObs = new ResizeObserver( entries => {
+const _jtcu_dom_resizeMap = new Map();
+const _jtcu_dom_resizeObs = new ResizeObserver( entries => {
 	entries.forEach( e => {
-		jtcu_dom_resizeMap.get( e.target )
+		_jtcu_dom_resizeMap.get( e.target )
 			.forEach( fn => fn( e.contentRect.width, e.contentRect.height ) );
 	} );
 } );
 
 function jtcu_dom_observeSize( el, fn ) {
-	if ( jtcu_dom_resizeMap.has( el ) ) {
-		jtcu_dom_resizeMap.get( el ).push( fn );
+	if ( _jtcu_dom_resizeMap.has( el ) ) {
+		_jtcu_dom_resizeMap.get( el ).push( fn );
 	} else {
-		jtcu_dom_resizeMap.set( el, [ fn ] );
+		_jtcu_dom_resizeMap.set( el, [ fn ] );
 	}
-	jtcu_dom_resizeObs.observe( el );
+	_jtcu_dom_resizeObs.observe( el );
 }
 
 function jtcu_dom_unobserveSize( el, fn ) {
-	const fns = jtcu_dom_resizeMap.get( el );
+	const fns = _jtcu_dom_resizeMap.get( el );
 	const fnInd = fns?.indexOf( fn );
 
 	if ( fnInd > -1 ) {
-		jtcu_dom_resizeObs.unobserve( el );
+		_jtcu_dom_resizeObs.unobserve( el );
 		fns.splice( fnInd, 1 );
 		if ( fns.length === 0 ) {
-			jtcu_dom_resizeMap.delete( el );
+			_jtcu_dom_resizeMap.delete( el );
 		}
 	}
 }
